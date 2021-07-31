4297b5e2ccc2edcda2e7cb7b58f5432fc68c7b8a
Arthur <artur.li@bk.ru>
2021-07-31 22:04:29 +0300

    #2 Auth - composer require laravel/ui:^2.4 - php artisan ui vue --auth

--- database/migrations/2021_07_31_080026_create_authors_table.php
+++ database/migrations/2021_07_31_080026_create_authors_table.php
@@ -8,16 +8,17 @@ class CreateAuthorsTable extends Migration
 {
     /**
      * Run the migrations.
-     *
      * @return void
      */
     public function up()
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('author_id');
+
             $table->timestamps();
         });
-    }
+    }1
 
     /**
      * Reverse the migrations.
