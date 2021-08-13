26b3368f2227ac0f07873170fef20c3907bec03f
Arthur <artur.li@bk.ru>
2021-08-13 23:06:13 +0300

    CRUD(adaptive) - Create(layout:form,Function) - Update(layout:form,Function) - Delete(layout:form,Function)

--- resources/views/admin/authors/index.blade.php
+++ resources/views/admin/authors/index.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 @section('content')
     <h1>Admin: Authors</h1>
-    <section class="admin-authors">
+    <section class="admin-authors container">
         <nav>
             <a href="{{route('library.admin.authors.create')}}">Add authors</a>
         </nav>
         <table>
+            <thead>
             <tr>
                 <th>ID</th>
                 <th>Name</th>
@@ -14,24 +15,27 @@
                 <th>Books name</th>
                 <th>Date Created</th>
             </tr>
-            @foreach($authors as $author)
-                <tr>
-                    <td>{{$author->id}}</td>
-                    <td>
-                        <a href="{{route('library.admin.authors.edit',$author->id)}}">
-                            {{$author->firstName}} {{$author->lastName}}</a>
-                    </td>
-                    <td>{{$author->dateBirth}}</td>
-                    <td>{{$author->books_count}}</td>
+            </thead>
+            <tboby>
+                @foreach($authors as $author)
+                    <tr>
+                        <td data-label="ID">{{$author->id}}</td>
+                        <td data-label="Name">
+                            <a href="{{route('library.admin.authors.edit',$author->id)}}">
+                                {{$author->firstName}} {{$author->lastName}}</a>
+                        </td>
+                        <td data-label="Date Birthday">{{$author->dateBirth}}</td>
+                        <td data-label="Books">{{$author->books_count}}</td>
 
-                    <td>
-                        @foreach($author->books as $book)
-                            <li>{{$book->bookname}}</li>
-                        @endforeach
-                    </td>
-                    <td>{{$author->created_at}}</td>
-                </tr>
-            @endforeach
+                        <td data-label="Books name">
+                            @foreach($author->books as $book)
+                                <li>{{$book->bookname}}</li>
+                            @endforeach
+                        </td>
+                        <td data-label="Date Created">{{$author->created_at}}</td>
+                    </tr>
+                @endforeach
+            </tboby>
         </table>
     </section>
 @endsection
